bf6e3a508d1e071728133876096101a196209a52
Pablito <Pablito@Pablito-PC>
2020-04-30 17:18:40 -0300

    Clase Main ceada Mensaje de Hola creado

---
+++ PruebaGitHub/src/prueba/PruebaExe.java
@@ -0,0 +1,12 @@
+package prueba;
+
+import javax.swing.JOptionPane;
+
+public class PruebaExe {
+
+	public static void main(String[] args) {
+	
+		JOptionPane.showMessageDialog(null, "Hola");
+	}
+
+}
